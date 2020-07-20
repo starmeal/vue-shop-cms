c98ca1b96d470a0b94f2237b3c9e894efef3f861
zjw <zhang_jw33@yeah.net>
2020-07-20 16:22:15 +0800

    提交代码

---
+++ src/components/city/cascader.vue
@@ -0,0 +1,373 @@
+<template>
+  <span class="container">
+    <!-- <el-cascader
+    clearable
+     v-if="value !== null"
+      :value="value"
+       :options="cityList"
+       @change="demochange"
+        :props="optionProps"></el-cascader> -->
+    <el-cascader
+     clearable
+      v-if="value !== null"
+       :size="size"
+        placeholder="请选择"
+        :options="cityList"
+         :props="optionProps"
+          v-model="value"
+           @change="handleChange" />
+    <el-cascader clearable v-else :size="size" placeholder="请选择" :options="cityList" :props="optionProps" v-model="value" @change="handleChange" />
+  </span>
+</template>
+
+<script>
+// 省市区json文件
+import city from './district.json';
+export default {
+  props: {
+    level: {
+      type: String,
+      default: ''
+    },
+    size: {
+      type: String,
+      default: 'mini'
+    },
+    cityvalue: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      options: [
+        {
+          value: 'zhinan',
+          label: '指南',
+          children: [
+            {
+              value: 'shejiyuanze',
+              label: '设计原则',
+              children: [
+                {
+                  value: 'yizhi',
+                  label: '一致'
+                },
+                {
+                  value: 'fankui',
+                  label: '反馈'
+                },
+                {
+                  value: 'xiaolv',
+                  label: '效率'
+                },
+                {
+                  value: 'kekong',
+                  label: '可控'
+                }
+              ]
+            },
+            {
+              value: 'daohang',
+              label: '导航',
+              children: [
+                {
+                  value: 'cexiangdaohang',
+                  label: '侧向导航'
+                },
+                {
+                  value: 'dingbudaohang',
+                  label: '顶部导航'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          value: 'zujian',
+          label: '组件',
+          children: [
+            {
+              value: 'basic',
+              label: 'Basic',
+              children: [
+                {
+                  value: 'layout',
+                  label: 'Layout 布局'
+                },
+                {
+                  value: 'color',
+                  label: 'Color 色彩'
+                },
+                {
+                  value: 'typography',
+                  label: 'Typography 字体'
+                },
+                {
+                  value: 'icon',
+                  label: 'Icon 图标'
+                },
+                {
+                  value: 'button',
+                  label: 'Button 按钮'
+                }
+              ]
+            },
+            {
+              value: 'form',
+              label: 'Form',
+              children: [
+                {
+                  value: 'radio',
+                  label: 'Radio 单选框'
+                },
+                {
+                  value: 'checkbox',
+                  label: 'Checkbox 多选框'
+                },
+                {
+                  value: 'input',
+                  label: 'Input 输入框'
+                },
+                {
+                  value: 'input-number',
+                  label: 'InputNumber 计数器'
+                },
+                {
+                  value: 'select',
+                  label: 'Select 选择器'
+                },
+                {
+                  value: 'cascader',
+                  label: 'Cascader 级联选择器'
+                },
+                {
+                  value: 'switch',
+                  label: 'Switch 开关'
+                },
+                {
+                  value: 'slider',
+                  label: 'Slider 滑块'
+                },
+                {
+                  value: 'time-picker',
+                  label: 'TimePicker 时间选择器'
+                },
+                {
+                  value: 'date-picker',
+                  label: 'DatePicker 日期选择器'
+                },
+                {
+                  value: 'datetime-picker',
+                  label: 'DateTimePicker 日期时间选择器'
+                },
+                {
+                  value: 'upload',
+                  label: 'Upload 上传'
+                },
+                {
+                  value: 'rate',
+                  label: 'Rate 评分'
+                },
+                {
+                  value: 'form',
+                  label: 'Form 表单'
+                }
+              ]
+            },
+            {
+              value: 'data',
+              label: 'Data',
+              children: [
+                {
+                  value: 'table',
+                  label: 'Table 表格'
+                },
+                {
+                  value: 'tag',
+                  label: 'Tag 标签'
+                },
+                {
+                  value: 'progress',
+                  label: 'Progress 进度条'
+                },
+                {
+                  value: 'tree',
+                  label: 'Tree 树形控件'
+                },
+                {
+                  value: 'pagination',
+                  label: 'Pagination 分页'
+                },
+                {
+                  value: 'badge',
+                  label: 'Badge 标记'
+                }
+              ]
+            },
+            {
+              value: 'notice',
+              label: 'Notice',
+              children: [
+                {
+                  value: 'alert',
+                  label: 'Alert 警告'
+                },
+                {
+                  value: 'loading',
+                  label: 'Loading 加载'
+                },
+                {
+                  value: 'message',
+                  label: 'Message 消息提示'
+                },
+                {
+                  value: 'message-box',
+                  label: 'MessageBox 弹框'
+                },
+                {
+                  value: 'notification',
+                  label: 'Notification 通知'
+                }
+              ]
+            },
+            {
+              value: 'navigation',
+              label: 'Navigation',
+              children: [
+                {
+                  value: 'menu',
+                  label: 'NavMenu 导航菜单'
+                },
+                {
+                  value: 'tabs',
+                  label: 'Tabs 标签页'
+                },
+                {
+                  value: 'breadcrumb',
+                  label: 'Breadcrumb 面包屑'
+                },
+                {
+                  value: 'dropdown',
+                  label: 'Dropdown 下拉菜单'
+                },
+                {
+                  value: 'steps',
+                  label: 'Steps 步骤条'
+                }
+              ]
+            },
+            {
+              value: 'others',
+              label: 'Others',
+              children: [
+                {
+                  value: 'dialog',
+                  label: 'Dialog 对话框'
+                },
+                {
+                  value: 'tooltip',
+                  label: 'Tooltip 文字提示'
+                },
+                {
+                  value: 'popover',
+                  label: 'Popover 弹出框'
+                },
+                {
+                  value: 'card',
+                  label: 'Card 卡片'
+                },
+                {
+                  value: 'carousel',
+                  label: 'Carousel 走马灯'
+                },
+                {
+                  value: 'collapse',
+                  label: 'Collapse 折叠面板'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          value: 'ziyuan',
+          label: '资源',
+          children: [
+            {
+              value: 'axure',
+              label: 'Axure Components'
+            },
+            {
+              value: 'sketch',
+              label: 'Sketch Templates'
+            },
+            {
+              value: 'jiaohu',
+              label: '组件交互文档'
+            }
+          ]
+        }
+      ],
+      value: null,
+      optionProps: {
+        value: 'adcode',
+        label: 'name',
+        children: 'districts'
+      }
+    };
+  },
+  methods: {
+    demochange(val) {
+      console.log(val);
+      // this.$emit('changecity', value);
+    },
+    // 递归处理空数组
+    initCityList(list) {
+      list.forEach(element => {
+        //   判断省市||省市区
+        if (this.level == 'city') {
+          if (element.level == 'city' || element.districts.length == 0) {
+            delete element.districts;
+            return false;
+          }
+        } else {
+          if (element.districts.length == 0) {
+            delete element.districts;
+            return false;
+          }
+        }
+        if (element.districts.length) {
+          this.initCityList(element.districts);
+        }
+      });
+    },
+    // 获取选中得值传出
+    handleChange(value) {
+      console.log(value);
+      this.$emit('changecity', value);
+    }
+  },
+  created() {
+    console.log(this.cityvalue);
+    //   初始化省市区联动
+    if (this.cityvalue.length == 0) {
+      if (this.level == 'city') {
+        this.value = '';
+      } else {
+        this.value = ['230000', '230100'];
+      }
+    } else {
+      this.value = null;
+      this.$nextTick(() => {
+        this.value = this.cityvalue;
+      });
+    }
+    this.cityList = JSON.parse(JSON.stringify(city.districts[0].districts));
+    this.initCityList(this.cityList);
+  },
+  components: {}
+};
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/views/store/index.vue
+++ src/views/store/index.vue
@@ -1,15 +1,118 @@
 <template>
-  <div>
-      i am 店铺首页
+  <div class="shopSettings">
+    <div class="containers">
+      <el-form :model="form" :rules="rules" ref="form" :size="size" label-width="200px" class="demo-form">
+        <el-form-item label="经营类目" prop="name">
+          {{form.name}}
+        </el-form-item>
+        <el-form-item label="店铺名称" prop="name1">
+          <el-input v-model="form.name1" placeholder="请输入店铺名称，建议15字以内"></el-input>
+        </el-form-item>
+        <el-form-item label="店铺标志" prop="">
+
+        </el-form-item>
+        <el-form-item label="店铺介绍" prop="name3">
+          <el-input v-model="form.name3"
+                    placeholder="请输入店铺的经营范围、经营品牌、售后承诺等相关的文字(入驻申请时的简介)"
+                    type="textarea"
+                    show-word-limit
+                    maxlength="200"
+                    rows="6"></el-input>
+        </el-form-item>
+        <el-form-item label="客服电话" prop="name5">
+          <el-input class="inputStyle1" v-model="form.name4" placeholder="区号"></el-input>
+          <el-input class="inputStyle2" v-model="form.name5" placeholder="如果输入手机号不填区号"></el-input>
+        </el-form-item>
+        <el-form-item label="经营地址" prop="name7">
+          <cascader level="city" :size="size" @changecity="changecity" />
+          <el-input v-model="form.name7"
+                    placeholder="填写详细地址"
+                    type="textarea"
+                    show-word-limit
+                    style="margin-top: 10px;"
+                    maxlength="100"
+                    rows="4"></el-input>
+        </el-form-item>
+        <el-form-item label="店招图片" prop="">
+
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('form')">保存</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
+  import cascader from './../../components/city/cascader'
 export default {
+  data(){
+    return {
+      size: 'mini',
+      form: {
+        name: '123',
+        name1: '',
+        name3: '',
+        name4: '',
+        name5: '',
+        name7: ''
+      },
+      rules: {
+        name1: [
+          { required: true, message: '请输入店铺名称', trigger: 'blur' }
+        ],
+        name3: [
+          { required: true, message: '请输入店铺介绍', trigger: 'blur' }
+        ],
+        name5: [
+          { required: true, message: '请输入客服电话', trigger: 'blur' }
+        ],
+        name7: [
+          { required: true, message: '请填写详细地址', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  components: {
+    cascader
+  },
+  methods: {
+    //城市选择
+    changecity(){
 
+    },
+    //保存
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('提交');
+        } else {
+          console.log('错误提交');
+          return false;
+        }
+      });
+    },
+  }
 }
 </script>
 
 <style>
-
+ .containers {
+    width: 98%;
+    margin: 0 auto;
+    padding: 20px 0px;
+    background: #fff;
+    border-radius: 4px;
+ }
+  .demo-form {
+    width: 85%;
+  }
+  .inputStyle1 {
+    width: 200px;
+    margin-right: 15px;
+  }
+  .inputStyle2 {
+    width: 465px;
+  }
 </style>
