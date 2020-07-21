033441caba79da7c888ff2bdffcafec5e66709f7
zjw <zhang_jw33@yeah.net>
2020-07-21 17:37:00 +0800

    提交代码

--- src/api/store/index.js
+++ src/api/store/index.js
@@ -3,7 +3,15 @@ import request from "@/utils/request";
 //店铺设置
 export function shopSettings(data) {
   return request({
-    url: "/shop/merchants/info/selectDetailInfo",
+    url: "/merchants/info/selectDetailInfo",
+    method: "post",
+    data: data
+  });
+}
+//编辑店铺设置
+export function editShopSettings(data) {
+  return request({
+    url: "/merchants/info/editInfo",
     method: "post",
     data: data
   });
