de02c70d3ff4fb12e6a660b66063c1b80b445e20
mrwang <511019998@qq.com>
2020-07-24 10:50:13 +0800

    '修改文件配置'

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router  from './router'
 import store from './store'
 import ElementUI from 'element-ui';
 // import 'element-ui/lib/theme-chalk/index.css';
+import '@/styl/element-variables.scss'
 import directive from '@/directives'
 import filter from '@/filters'
 import '@/router/permission.js'
